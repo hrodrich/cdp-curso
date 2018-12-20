82d6adcb14dc6a75db95be40948e8d3d9515a374
hrodrich <hrodrich.26@gmail.com>
2018-12-20 10:49:38 -0600

    Primer ejemplo API

--- JavaScript/js/app.js
+++ JavaScript/js/app.js
@@ -1,9 +1,46 @@
+/*
 function new_element(){
     $("body").append("<p>Java</p>");
     $("body").append("<p>Java</p>");
     $("body").append("<p>Java</p>");
 }
+*/
 
+/*
 $("#green_button").click(function() {
     alert("Handler for .click() called.");
+});
+*/
+
+
+$("#dar_click").click(function() {
+    $("input").before("<p>Aquí da click</p>");
+});
+
+
+//arreglo JavaScript
+var frutas = ["manzana", "piña", "sandía", "limón"];
+
+//Jquery
+$.each(frutas, function(numero_fruta, nombre_fruta){
+    $("#lista_fruta").append("<li>"+nombre_fruta+"</li>");
+    console.log(nombre_fruta);
+});
+
+$("#dar_click_usuarios").click(function () {
+    //alert("Funciona!");
+    //https://jsonplaceholder.typicode.com/users
+
+    $.ajax({
+        url:"https://jsonplaceholder.typicode.com/users",
+        type: "get",
+        dataType: "json",
+        success: function(respuesta_servidor){
+            console.log(respuesta_servidor);
+            $.each(respuesta_servidor, function(numero_usuario, usuario){
+                console.log(usuario)
+                $("#log_usuario").append("<li>"+usuario.username +", "+ usuario.email+"</li>");
+            });
+        }
+    });
 });
