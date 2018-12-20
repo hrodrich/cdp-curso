6486310b0c90bc59657f22a0b2fdcb644ac34606
hrodrich <hrodrich.26@gmail.com>
2018-12-20 08:16:04 -0600

    Clase de javascript

---
+++ JavaScript/js/app.js
@@ -0,0 +1,9 @@
+function new_element(){
+    $("body").append("<p>Java</p>");
+    $("body").append("<p>Java</p>");
+    $("body").append("<p>Java</p>");
+}
+
+$("#green_button").click(function() {
+    alert("Handler for .click() called.");
+});
